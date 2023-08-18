cfa87f5a6c6d6bb83d91ef83390a170cbd081203
kislaysr <745kishlay.sr@gmail.com>
2023-08-19 01:49:47 +0530

    removed firebase + added amplify

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,28 @@
-import { initializeApp } from "firebase/app"
-import { getAnalytics } from "firebase/analytics"
-import { getFirestore } from "firebase/firestore"
-import { getDocs, collection } from "firebase/firestore"
+import { Amplify } from "aws-amplify"
+import { Authenticator, Button } from "@aws-amplify/ui-react"
+import "@aws-amplify/ui-react/styles.css"
 import { Layout } from "antd"
 import Dashboard from "./containers/Dashboard"
-import { useEffect } from "react"
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyA12h1NS3TPB2BqwOds0v9_GYJlM8OpC48",
-//   authDomain: "convobus.firebaseapp.com",
-//   projectId: "convobus",
-//   storageBucket: "convobus.appspot.com",
-//   messagingSenderId: "1067562646321",
-//   appId: "1:1067562646321:web:b59f94c94b5d9e560d8e10",
-//   measurementId: "G-QMP8ZN16JK",
-// }
+Amplify.configure({
+  Auth: {
+    region: "us-east-1",
+    userPoolId: "us-east-1_rk0oC8SGz",
+    userPoolWebClientId: "hk2tm51l5cri0gpgr77q02tjs",
+  },
+})
 
 function App() {
-  // useEffect(() => {
-  //   const app = initializeApp(firebaseConfig)
-  //   const analytics = getAnalytics(app)
-  //   const database = getFirestore(app)
-
-  //   const collection_name = "test1"
-
-  //   getDocs(collection(database, collection_name)).then((resp) =>
-  //     console.log("####", JSON.parse(JSON.stringify(resp))),
-  //   )
-  // }, [])
-
   return (
-    <Layout style={{ height: "100vh" }}>
-      <Dashboard />
-    </Layout>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <Layout style={{ height: "100vh" }}>
+          <h1>Hello {user?.username}</h1>
+          <Button onClick={signOut}>Sign out</Button>
+          <Dashboard />
+        </Layout>
+      )}
+    </Authenticator>
   )
 }
 
